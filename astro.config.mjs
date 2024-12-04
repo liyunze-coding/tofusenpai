0684e8971f927418bfc8a1df6bd84e8798834ba9
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2024-12-05 06:52:45 +0800

    deploy to thewilliamyao

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://thewilliamyao.com",
 	integrations: [react(), tailwind()],
 	vite: {
 		css: {
