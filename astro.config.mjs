fb81a26eef6f42a3a54c56dab8502a20a362f215
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2025-05-04 19:51:02 +1000

    disable sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService  } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -17,4 +17,7 @@ export default defineConfig({
 			},
 		},
 	},
+	image: {
+		service: passthroughImageService()
+	}
 });
